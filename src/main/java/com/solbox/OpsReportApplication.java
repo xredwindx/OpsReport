18eeb3a53982a8aa6703283d8795348ff1014980
sichoon.kwon <sichoon.kwon@solbox.com>
2017-10-13 18:25:44 +0900

    redis

--- src/main/java/com/solbox/OpsReportApplication.java
+++ src/main/java/com/solbox/OpsReportApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableRedisHttpSession
 public class OpsReportApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OpsReportApplication.class, args);
